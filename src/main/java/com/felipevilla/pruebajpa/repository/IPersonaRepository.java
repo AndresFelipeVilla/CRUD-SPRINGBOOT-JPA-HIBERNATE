24b3c80f6de22ff5dca205322478ff3a92725ae4
Felipe villa <andresfelipevillapardo7@gmail.com>
2024-11-28 17:05:07 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/felipevilla/pruebajpa/repository/IPersonaRepository.java
@@ -0,0 +1,1 @@
+package com.felipevilla.pruebajpa.repository;import com.felipevilla.pruebajpa.model.Persona;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface IPersonaRepository extends JpaRepository <Persona, Long> {}
---
+++ src/test/java/com/felipevilla/pruebajpa/PruebajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipevilla.pruebajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
